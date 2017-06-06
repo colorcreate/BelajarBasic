a4c222d222e2e12f8b41f5430729210a1811315f
colorcreate <ibrohim.syarif@gmail.com>
2017-06-06 20:46:08 +0700

    C# Program to Campare Two Dates

---
+++ 2. Compare Two Dates/2. Compare Two Dates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Compare_Two_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date1 = new DateTime(2017, 06, 06);
+            Console.WriteLine("Starting date\t: {0}", date1);
+            DateTime date2 = date1.AddDays(10);
+            Console.WriteLine("Ending date\t: {0}", date2);
+
+            Console.WriteLine("{0} terjadi sebelum {1}", date1, date2);
+
+        }
+    }
+}
--- 2. Date Format/2. Date Format/Program.cs
+++ 2. Date Format/2. Date Format/Program.cs
@@ -16,7 +16,7 @@ namespace _2.Date_Format
             Console.WriteLine(date.ToString("yyyy-MM-dd"));
             Console.WriteLine(date.ToString("dd-MM-yy"));
             Console.WriteLine(date.ToString("M/d/yyyy"));
-            Console.WriteLine(date.ToString("M/d/yy"));
+            Console.WriteLine(date.ToString("MM"));
             Console.WriteLine(date.ToString("MM/dd/yyyy"));
             Console.WriteLine(date.ToString("MM/dd/yy"));
             Console.WriteLine(date.ToString("yy/MM/dd"));
