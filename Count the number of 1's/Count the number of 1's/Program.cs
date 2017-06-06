1e039b4125151bc0944f3236ae9fb6dc326b6363
colorcreate <ibrohim.syarif@gmail.com>
2017-06-06 19:29:09 +0700

    C# Program to Count the Number of 1's in the Entered Number

---
+++ Count the number of 1's/Count the number of 1's/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_the_number_of_1_s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan nilai n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            
+            int number, count = 0;
+            for (var i=0; i<n; i++)
+            {
+                Console.Write("Masukkan angka : ");
+                number = int.Parse(Console.ReadLine());
+                if (number == 1) count++;
+            }
+
+            Console.WriteLine("Anda telah memasukkan angka 1 sebanyak " + count+ " buah");
+        }
+    }
+}
