ac8f4e2ff061951e118ff19b16009884c80adcfb
colorcreate <ibrohim.syarif@gmail.com>
2017-06-06 19:10:58 +0700

    input integer

---
+++ Binary Triangle/Binary Triangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i=0; i<n; i++)
+            {
+                for (int j=0; j<=i; j++)
+                {
+                    Console.Write((i+j)%2==0 ? 1:0);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
