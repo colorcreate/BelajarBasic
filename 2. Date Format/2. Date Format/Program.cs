87c9cbb484d3cca216f9a8396ecc782628ea99eb
colorcreate <ibrohim.syarif@gmail.com>
2017-06-06 20:30:15 +0700

    C# Program to Display the Date in Various Formats

---
+++ 2. Date Format/2. Date Format/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Date_Format
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(2013, 6, 23);
+            Console.WriteLine("Some Date Formats : ");
+            Console.WriteLine("Date and Time:  {0}", date);
+            Console.WriteLine(date.ToString("yyyy-MM-dd"));
+            Console.WriteLine(date.ToString("dd-MM-yy"));
+            Console.WriteLine(date.ToString("M/d/yyyy"));
+            Console.WriteLine(date.ToString("M/d/yy"));
+            Console.WriteLine(date.ToString("MM/dd/yyyy"));
+            Console.WriteLine(date.ToString("MM/dd/yy"));
+            Console.WriteLine(date.ToString("yy/MM/dd"));
+            Console.Read();
+        }
+    }
+}
