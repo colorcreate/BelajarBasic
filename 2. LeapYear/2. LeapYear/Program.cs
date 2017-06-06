cab4476d064928db4792dc5e4707edc5a56a6cf2
colorcreate <ibrohim.syarif@gmail.com>
2017-06-06 20:11:28 +0700

    C# Program to Check Whether the Entered Year is a Leap Year or Not

---
+++ 2. LeapYear/2. LeapYear/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan tahun yang akan dicek: ");
+            int year = int.Parse(Console.ReadLine());
+
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine("{0} adalah tahun kabisat", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} adalah bukan tahun kabisat", year);
+            }
+        }
+    }
+}
