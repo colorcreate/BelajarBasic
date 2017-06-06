4c26c3d6c5a6ced2e17543261d37afa5ed1b19c6
colorcreate <ibrohim.syarif@gmail.com>
2017-06-06 17:40:36 +0700

    as

---
+++ Number is Even or Odd/Number is Even or Odd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_is_Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.Write("Masukkan angka : ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+                Console.WriteLine("\n"+ number + " adalah bilangan genap");
+            else
+                Console.WriteLine("\n"+ number + " adalah bilangan ganjil");
+        }
+    }
+}
