d52d2771101df365026cf4fe128f13fa5ec9e069
colorcreate <ibrohim.syarif@gmail.com>
2017-06-08 15:33:33 +0700

    coba ganti nama branch

---
+++ 2. Generate random number/2. Generate random number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Generate_random_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write("ketik 1 untuk generate bilangan random, ketik 0 untuk quit: ");
+                break;
+            }          
+        }
+        Random r = new Random();
+
+        static void generate()
+        {
+
+        }
+
+    }
+}
